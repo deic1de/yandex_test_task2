a5c1c69ca475b8cfc6dc09f704f1cd0d297ac31a
deic1de <de1c1de2life@gmail.com>
2016-07-06 21:53:54 +0300

    test

---
+++ index.js
@@ -0,0 +1,61 @@
+/**
+  * @param {Array} data – массив CSS классов
+  */
+var data = ["pzhoccuf","pzhoccuf","pzhoccuf","pzhoccuf","pzhoccuf","pzhoccuf","pzhoccuf","pzhoccuf","pzhoccuf","pzhoccuf","pzhoccuf","pzhoccuf","hlkon","hlkon","hlkon","hlkon","hlkon","gczswo","gczswo","gczswo","gczswo","gczswo","jryueaq","jryueaq","jryueaq","jryueaq","jryueaq","rbqzaoj","rbqzaoj","rbqzaoj","rbqzaoj","rbqzaoj","rbqzaoj","rbqzaoj","rbqzaoj","rbqzaoj","rbqzaoj","rbqzaoj","gptkf","gptkf","gptkf","gptkf","gptkf","gptkf","gptkf","gptkf","gptkf","gptkf","gptkf","gptkf","gptkf","gptkf","gptkf","qmszpno","qmszpno","qmszpno","qmszpno","qmszpno","dkeozlr","dkeozlr","dkeozlr","dkeozlr","dkeozlr","fhirfdqg","fhirfdqg","thnsypuk","thnsypuk","thnsypuk","thnsypuk","thnsypuk","thnsypuk","thnsypuk","thnsypuk","thnsypuk","thnsypuk","thnsypuk","thnsypuk","zfwhsm","zfwhsm","zfwhsm","zfwhsm","zfwhsm","zfwhsm","zfwhsm","zfwhsm","zfwhsm","zfwhsm","ddrlqkxm","ddrlqkxm","ddrlqkxm","ddrlqkxm","ddrlqkxm","ddrlqkxm","hyxjvp","hyxjvp","hyxjvp","hyxjvp","hyxjvp","hyxjvp","hyxjvp","hyxjvp","hyxjvp","hyxjvp","yvhwgozu","yvhwgozu","yvhwgozu","yvhwgozu","yvhwgozu","yvhwgozu","yvhwgozu","yvhwgozu","yvhwgozu","yvhwgozu","yvhwgozu","yvhwgozu","yvhwgozu","yvhwgozu","drmkwvdp","drmkwvdp","drmkwvdp","drmkwvdp","drmkwvdp","drmkwvdp","drmkwvdp","wqlsz","wqlsz","wqlsz","wqlsz","wqlsz","wqlsz","wqlsz","awajl","awajl","awajl","ajjmwx","ajjmwx","ajjmwx","ajjmwx","ajjmwx","ajjmwx","ajjmwx","ajjmwx","ajjmwx","ajjmwx","iwqyw","iwqyw","iwqyw","iwqyw","hkffs","hkffs","hkffs","hkffs","hkffs","hkffs","ifdlsscs","ifdlsscs","ifdlsscs","ifdlsscs","ifdlsscs","ifdlsscs","mffib","mffib","tuxpbx","tuxpbx","tuxpbx","tuxpbx","tuxpbx","tuxpbx","tuxpbx","tuxpbx","tuxpbx","tuxpbx","tuxpbx","nrlsku","nrlsku","nrlsku","nrlsku","nrlsku","godpap","godpap","godpap","godpap","bxufjd","bxufjd","bxufjd","bxufjd","bxufjd","bxufjd","bxufjd","bxufjd","bxufjd","bxufjd","bxufjd","bxufjd","ayptks","ayptks","ayptks","ayptks","ayptks","ayptks","ayptks","ayptks","ayptks","ayptks","ayptks","ayptks","ayptks","ayptks","ayptks","dvsfalb","dvsfalb","dvsfalb","dvsfalb","dvsfalb","dvsfalb","dvsfalb","dvsfalb","dvsfalb","dvsfalb","dvsfalb","ynkyd","ynkyd","ynkyd","tutdetfh","tutdetfh","tutdetfh","tutdetfh","tutdetfh","tutdetfh","tutdetfh","bwohn","bwohn","bwohn","bwohn","fobrh","fobrh","fobrh","fobrh","fobrh","fobrh","fobrh","fobrh","fobrh","qlftsqz","qlftsqz","qlftsqz","qlftsqz","qlftsqz","qlftsqz","qlftsqz","qlftsqz","qlftsqz","cdudq","cdudq","cdudq","cdudq","cdudq","cdudq","cdudq","cdudq","cdudq","cdudq","cdudq","sinvb","sinvb","sinvb","sinvb","sinvb","sinvb","sinvb","sinvb","sinvb","sinvb","sinvb","sinvb","sinvb","sinvb","sinvb","oepixgva","oepixgva","oepixgva","oepixgva","oepixgva","oepixgva","oepixgva","oepixgva","oepixgva","oepixgva","oepixgva","oepixgva","oepixgva","oepixgva","vcpzalfs","vcpzalfs","vcpzalfs","vcpzalfs","vcpzalfs","vcpzalfs","qgwxeq","qgwxeq","qgwxeq","qgwxeq","qgwxeq","qgwxeq","qgwxeq","qgwxeq","qgwxeq","qgwxeq","qgwxeq","qgwxeq","hkfwqvod","hkfwqvod","hkfwqvod","hkfwqvod","hkfwqvod","hkfwqvod","hkfwqvod","hkfwqvod","hkfwqvod","hkfwqvod","hkfwqvod","hkfwqvod","hwsmy","hwsmy","hwsmy","hwsmy","hwsmy","hwsmy","hwsmy","hwsmy","hwsmy","hwsmy","hwsmy","hwsmy","hwsmy","hwsmy","hwsmy","oejrmxvx","oejrmxvx","wbjuqcyk","wbjuqcyk","wbjuqcyk","wbjuqcyk","wbjuqcyk","wbjuqcyk","xfoenl","xfoenl","xfoenl","xfoenl","eglubqpl","eglubqpl","eglubqpl","eglubqpl","eglubqpl","eglubqpl","eglubqpl","eglubqpl","eglubqpl","caqxeu","caqxeu","caqxeu","caqxeu","caqxeu","caqxeu","caqxeu","caqxeu","fqhbjmmd","fqhbjmmd","tskqb","tskqb","tskqb","tskqb","tskqb","tskqb","tskqb","tskqb","tskqb","tskqb","tskqb","tskqb","tskqb","tskqb","uayjdlnz","uayjdlnz","uayjdlnz","uayjdlnz","uayjdlnz","uayjdlnz","uayjdlnz","uayjdlnz","uayjdlnz","uayjdlnz","wdedzl","wdedzl","wdedzl","wdedzl","wdedzl","wdedzl","wdedzl","wdedzl","wdedzl","wdedzl","wdedzl","wdedzl","wdedzl","wdedzl","slvzehcr","slvzehcr","slvzehcr","slvzehcr","slvzehcr","slvzehcr","slvzehcr","slvzehcr","slvzehcr","slvzehcr","ahvifj","ahvifj","ahvifj","ahvifj","ahvifj","qkzhzgr","qkzhzgr","qkzhzgr","qkzhzgr","qkzhzgr","qkzhzgr","qkzhzgr","qkzhzgr","qkzhzgr","qkzhzgr","qkzhzgr","qkzhzgr","qkzhzgr","qkzhzgr","qkzhzgr","ylkqa","ylkqa","ylkqa","ylkqa","ylkqa","ylkqa","ylkqa","ylkqa","ylkqa","ylkqa","ylkqa","ylkqa","ylkqa","ylkqa","ylkqa","chfqdq","chfqdq","chfqdq","chfqdq","chfqdq","chfqdq","chfqdq","chfqdq","chfqdq","chfqdq","chfqdq","chfqdq","ylrrzhg","ylrrzhg","ylrrzhg","ylrrzhg","ylrrzhg","ylrrzhg","ylrrzhg","ylrrzhg","ylrrzhg","ylrrzhg","ylrrzhg","ylrrzhg","ylrrzhg","ylrrzhg","xvgrws","xvgrws","xvgrws","xvgrws","xvgrws","xvgrws","imbznmd","imbznmd","imbznmd","imbznmd","imbznmd","imbznmd","imbznmd","imbznmd","imbznmd","imbznmd","imbznmd","imbznmd","gnfjavnw","gnfjavnw","sngpjyr","sngpjyr","sngpjyr","ftfdov","ftfdov","joddy","joddy","joddy","joddy","joddy","joddy","joddy","opawo","opawo","opawo","opawo","opawo","opawo","opawo","opawo","smljvd","smljvd","smljvd","smljvd","smljvd","smljvd","eyptyacf","eyptyacf","eyptyacf","eyptyacf","eyptyacf","eyptyacf","eyptyacf","eyptyacf","eyptyacf","eyptyacf","qqhhk","qqhhk","qqhhk","qqhhk","hisfrxss","hisfrxss","hisfrxss","hisfrxss","hisfrxss","hisfrxss","hisfrxss","hisfrxss","hisfrxss","hisfrxss","uskfoipw","uskfoipw","uskfoipw","uskfoipw","uskfoipw","uskfoipw","uskfoipw","cuztpc","cuztpc"];
+
+module.exports = function(data) {
+  var ALPHABET = "qwertyuiopasdfghjklzxcvbnm0123456789_-".split("");
+  var DIGITS = "123456789_-".split("");
+
+  var classesCount = data.reduce(function(acc, className) {
+      if (!(className in acc)) {
+        acc[className] = 0;
+      }
+      acc[className] += 1;
+      return acc;
+    }, {}); /*Подсчет сколько раз используется тот или иной класс в приходящем массиве data */
+
+    var classCountArray = Object.keys(classesCount).map (function(className) {
+          return [className, classesCount[className]];
+    });  /*Перевод получившегося в результате предыдущего действия объекта в массив */
+
+    var classCountArraySorted = classCountArray.sort(function(classA, classB) {
+            return classB[1] - classA[1];
+          });  /*Сортировка массива по количеству повторяющихся данных */
+    console.log(classCountArraySorted);
+
+    var symbolCounter = Math.ceil(Math.log(classCountArraySorted.length) / Math.log(ALPHABET.length));
+    /*Количество символов необходимых для перенаименования классов количеством data.length */
+
+    var newClassNames = [];
+      for (i=0; i<26; i++){
+        newClassNames.push(ALPHABET[i]);
+      }
+      for (i=0; i < 26; i++){
+        for (j=0; j<ALPHABET.length; j++){
+          newClassNames.push(ALPHABET[i]+ALPHABET[j]);
+        }
+      }
+      for (i=0; i < 26; i++){
+        for (j=0; j<ALPHABET.length; j++){
+          for(k=0; k<ALPHABET.length; k++){
+            newClassNames.push(ALPHABET[i]+ALPHABET[j]+ALPHABET[k]);
+          }
+        }
+      }
+      newClassNames.length = classCountArraySorted.length;
+      /* Создание массива с новыми уникальными именами, количество объектов в массиве равняется количеству уникальных значений в data */
+
+      var _generateNewClassName = function(n) {
+        return newClassNames[n];
+      } /* Генератор нового имени в зависимости от старого индекса */
+
+      var result = classCountArraySorted.reduce(function(acc, classSort, index) {
+          var className = classSort[0];
+          acc[className] = _generateNewClassName(index);
+          return acc;
+
+        }, {});
+        /* Генерация объекта в котором ключом будет являться старое имя класса, а значением новое */
+        console.log(result);
+};
